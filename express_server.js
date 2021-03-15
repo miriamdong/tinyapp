10c45185e2dbd9b10743119cebdbacb7667e41f5
miriamdong <miriamdong@gmai.com>
2021-03-15 19:48:14 +0000

    Adding a Second Route and Template

--- express_server.js
+++ express_server.js
@@ -9,6 +9,21 @@ const urlDatabase = {
 
 app.set("view engine", "ejs");
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/hello", (req, res) => {
+  const templateVars = { greeting: 'Hello World!' };
+  res.render("hello_world", templateVars);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
