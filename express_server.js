6aa845c6e966662a95357488a01bce12e0cf5ace
miriamdong <miriamdong@gmai.com>
2021-03-17 02:17:31 +0000

    fix cookies

--- express_server.js
+++ express_server.js
@@ -1,22 +1,24 @@
 const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-app.use(cookieParser());
-// const {
-//   request
-// } = require("express");
-
 const PORT = 8080; // default port 8080
-app.use(bodyParser.urlencoded({
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const moment = require('moment');
+const path = require('path');
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({
   extended: true
 }));
-// eslint-disable-next-line func-style
-function generateRandomString() {
+app.set("view engine", "ejs");
+
+
+// generate random Id and short URL
+const generateRandomString = () => {
   let r = Math.random().toString(36).substring(6);
   return r;
-}
-
+};
 
 
 const urlDatabase = {
@@ -24,27 +26,142 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
 
+// Create a GET /register endpoint that responds form template.
+app.get('/register', (req, res) => {
+  const templateVars = {
+    userId: req.cookies["userId"],
+    email: req.cookies["email"],
+    password: req.cookies["password"],
+  };
+  res.render("register", templateVars);
+});
 
-app.set("view engine", "ejs");
+app.post('/register', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  if (!email || !password) {
+    return res.status(400).json({
+      msg: 'Please include an email address & password'
+    });
+  } else
+    for (const user in users) {
+      const found = Object.values(users[user]).includes(email);
+      if (!found) {
+        const id = generateRandomString();
+        users[id] = {
+          userId: id,
+          email: email,
+          password: password
+        };
+        res.cookie('userId', id);
+        // res.cookie('email', email);
+        res.redirect('/urls');
+        return users;
+      } else if (found) {
+        res.status(400).json({
+          msg: "Email already exists"
+        });
+      }
+    }
+});
+
+// Create a GET /login endpoint that responds with this new login form template.
+app.get('/login', (req, res) => {
+  const userId = req.cookies["userId"];
+  let currentUser = users[userId];
+  if (!userId) currentUser = false;
+  const templateVars = {
+    userId: userId,
+    email: currentUser["email"],
+    password: req.cookies["password"],
+  };
+  res.render("login", templateVars);
+});
+
+app.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  for (const user in users) {
+    const found = Object.values(users[user]).includes(email);
+    if (found) {
+      const currentUser = users[user];
+      console.log(currentUser);
+      if (currentUser.password === password) {
+        const userId = currentUser.id;
+        res.cookie('userId', userId);
+        res.redirect('/urls');
+      } else {
+        res.status(403).json({
+          msg: "password doesn't match"
+        });
+      }
+    } else {
+      res.status(403).json({
+        msg: `No user with email ${ req.body.email }`
+      });
+    }
+  }
+
+});
+
+// const checkEmail = (obj, email) => {
+//   for (const key in obj) {
+//     if (Object.values(obj[key]).includes(email));
+//     return true;
+//   } return false;
+// };
 
 app.get("/urls", (req, res) => {
+  const userId = req.cookies["userId"];
+
+  let currentUser = users[userId];
+  // console.log(currentUser);
+  if (!userId) currentUser = false;
   const templateVars = {
-    username: req.cookies["username"],
-    urls: urlDatabase
+    email: currentUser["email"],
+    userId: userId,
+    urls: urlDatabase,
+    user: currentUser
   };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const userId = req.cookies["userId"];
+  let currentUser = users[userId];
+  if (!userId) currentUser = false;
+  console.log(currentUser["email"]);
+  // console.log(currentUser);
+  const templateVars = {
+    email: currentUser["email"],
+    userId: userId,
+    urls: urlDatabase,
+    user: currentUser
+  };
+
+  res.render("urls_new", templateVars);
 });
+
+
 
 
 app.post("/urls", (req, res) => {
   // check the longURL is inside the urlDatabase
   const longURL = req.body.longURL.includes("http") ? longURL : `http://${ req.body.longURL }`;
-
   //if not, generate one to add to the urlDatabase, and redirect to (`/urls/:${ shortURL }`);
   if (!Object.values(urlDatabase).includes(longURL)) {
     const shortURL = generateRandomString();
@@ -67,7 +184,15 @@ app.post('/urls/:shortURL/delete', (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
+
+  const userId = req.cookies["userId"];
+  let currentUser = users[userId];
+  if (!userId) currentUser = false;
+  const email = currentUser["email"];
+
   const templateVars = {
+    userId: userId,
+    email: email,
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL]
   };
@@ -93,26 +218,16 @@ app.get("/u/:shortURL", (req, res) => {
   }
 });
 
-app.post("/login", (req, res) => {
-  // console.log(req.body.username);
-  res.cookie('username', req.body.username);
-  res.redirect('/urls');
-
-});
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('username', req.body.username);
+  console.log(req.body);
+  res.clearCookie('userId', req.body.userId);
   res.redirect('/urls');
 });
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
-
-
-// const templateVars = {
-//     username: req.cookies["username"],
-//   };
-
-// res.render("urls_index", templateVars);
